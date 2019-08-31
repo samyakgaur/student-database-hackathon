9f6bd0163edfb397eae38b20d5839f5285147010
Samyak Gaur <samyakgaur@yahoo.com>
2019-08-31 09:43:09 +0530

    Added Comments

--- student_website/first_app/admin.py
+++ student_website/first_app/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 from first_app.models import Topic,Entries
 
+admin.site.site_header = 'Student Database'
 admin.site.register(Topic)
 admin.site.register(Entries)
 
@@ -12,4 +13,10 @@ admin.site.register(Entries)
 Username : samyakgaur
 Email Id: samyakgaur@yahoo.com
 Password: @Samyak
+"""
+
+"""
+Username : testuser
+Email Id: NA
+Password: @Samyakgaur
 """
